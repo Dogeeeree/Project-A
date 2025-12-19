1cfb45a18f76d0b666bd76dc8899f24c44f5274e
Doger <9.2nguyenminhkhang14@gmail.com>
2025-12-19 12:45:05 +0700

    Edit index

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+header("Content-Type: application/json");
+echo json_encode([
+    "status" => "success",
+    "message" => "Backend PHP InfinityFree OK"
+    echo "message" => "Backend PHP InfinityFree OK "
+]);
