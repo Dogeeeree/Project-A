a15a565f655a43bbafcf10732d84f9f73dec5722
Doger <9.2nguyenminhkhang14@gmail.com>
2025-12-19 13:02:27 +0700

    Missing files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Docker PHP Test</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Họ tên:  - MSSV: ";
+        ?>
+    </h1>
+</body>
+</html>
